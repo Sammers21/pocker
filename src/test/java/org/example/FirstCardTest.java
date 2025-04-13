248321375c95fe92af0c8785e465335c6a4daafc
Sammers21 <titantins@gmail.com>
2025-04-13 22:16:14 +0300

    add tests

---
+++ src/test/java/org/example/FirstCardTest.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstCardTest {
+
+    @Test
+    public void firstCardIsCorrect() throws IOException {
+        Path directory = Paths.get("imgs_marked");
+        List<File> imgs = new ArrayList<>();
+        Files.list(directory)
+                .filter(Files::isRegularFile)
+                .filter(path -> path.toString().endsWith(".png"))
+                .forEach(path -> {
+                    imgs.add(path.toFile());
+                });
+        assertTrue(imgs.size() > 0, "Empty image set");
+        List<Boolean> testReuslts = imgs.stream().map(this::testFile).toList();
+        int falses = 0;
+        for (Boolean testReuslt : testReuslts) {
+            if (!testReuslt) {
+                falses++;
+            }
+        }
+        assertTrue(falses == 0, "There are '" + falses + "' tests with wrong answer and '"
+                + (testReuslts.size() - falses) + "' with right answer");
+    }
+
+    public Boolean testFile(File file) {
+        String name = file.getName();
+        System.out.println("File name: " + name);
+        String firstCard = name.substring(0, 2);
+        System.out.println("first card name:" + firstCard);
+        String recognizedName = "";
+        try {
+            recognizedName = Main.recognizeText(file).substring(0, 2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            assertTrue(false, "Failed with testing file " + name);
+        }
+        boolean equals = recognizedName.equals(firstCard);
+        return equals;
+    }
+}
