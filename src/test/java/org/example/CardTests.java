ac0f73db5f3064357ce3461e70ba1adcfce72d6e
Sammers21 <titantins@gmail.com>
2025-04-14 19:23:54 +0300

    add all cards test 0/361

--- src/test/java/org/example/CardTests.java
+++ src/test/java/org/example/CardTests.java
@@ -11,13 +11,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FirstCardTest {
+public class CardTests {
 
     @Test
     public void firstCardIsCorrect() throws IOException {
+        testCardCorrection(this::testFirstCardCorrect);
+    }
+
+    @Test
+    public void allCardsAreCorrect() throws IOException {
+        testCardCorrection(this::testAllCardsCorrect);
+    }
+
+    private void testCardCorrection(Function<File, Boolean> f) throws IOException {
         Path directory = Paths.get("imgs_marked");
         List<File> imgs = new ArrayList<>();
         Files.list(directory)
@@ -27,7 +37,7 @@ public class FirstCardTest {
                     imgs.add(path.toFile());
                 });
         assertTrue(imgs.size() > 0, "Empty image set");
-        List<Boolean> testReuslts = imgs.stream().map(this::testFile).toList();
+        List<Boolean> testReuslts = imgs.stream().map(f).toList();
         int falses = 0;
         for (Boolean testReuslt : testReuslts) {
             if (!testReuslt) {
@@ -38,7 +48,25 @@ public class FirstCardTest {
                 + (testReuslts.size() - falses) + "' with right answer");
     }
 
-    public Boolean testFile(File file) {
+    public Boolean testAllCardsCorrect(File file) {
+        String name = file.getName();
+        System.out.println("Testing file: " + name);
+        String cards = name.split("\\.")[0];
+        String recognizedName = "";
+        try {
+            recognizedName = Main.recognizeText(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false, "Failed with testing file " + name);
+        }
+        boolean equals = recognizedName.equals(cards);
+        if (!equals) {
+            System.out.println("Expected: " + cards + ", but got: " + recognizedName + " for file: " + name);
+        }
+        return equals;
+    }
+
+    public Boolean testFirstCardCorrect(File file) {
         String name = file.getName();
         int end = 2;
         if (name.startsWith("1")) {
